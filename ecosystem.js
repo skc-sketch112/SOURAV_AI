0f4e217b39235320c387def4ec5c471af7eeaf17
skc-sketch112 <chunnachud@gmail.com>
2025-10-07 08:53:44 +0530

    Create ecosystem.js

---
+++ ecosystem.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "SOURAV_MD_BOT",
+      script: "index.js",
+      watch: true,              // watches for local file changes for hot reload
+      ignore_watch: ["node_modules", "remote_plugins"], // don’t watch huge directories
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+        DEBUG_MODE: "true",
+        PLUGIN_REPO: "https://github.com/yourname/private-plugins.git",
+        GITHUB_TOKEN: "" // optional if private repo
+      },
+      autorestart: true,
+      max_restarts: 10,
+      restart_delay: 5000
+    }
+  ]
+};
